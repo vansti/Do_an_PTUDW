ddc000f9fc972a58c26713398725ab6b3b4a4daa
Trần Nhật Tâm <nhattam231297@gmail.com>
2018-06-17 21:44:11 +0700

    file controller

---
+++ controllers/managerController.js
@@ -0,0 +1,196 @@
+var express = require('express');
+var router = express.Router();
+var multer = require('multer');
+var xu_ly = require('../fn/xu_ly');
+var restrict = require('../middle-wares/restrictManager');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './public/themes/images/products/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png')
+        cb(null, true)
+    else {
+        req.fileValidationError = 'Error! Wrong mimetype picture of product ( just .jpg and .png only). Please reup the product';
+        return cb(null, false, new Error('goes wrong on the mimetype'));
+    }
+};
+
+const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter
+});
+
+router.get('/login', (req, res) => {
+    res.render('manager/login')
+});
+
+router.post('/login', (req, res) => {
+    var user = {
+        Email: req.body.Email,
+        Mat_khau: SHA256(req.body.Mat_khau).toString()
+    };
+
+    xu_ly.connectDatabase().query("select * from quan_ly where Email = ? AND Mat_khau = ?", [user.Email, user.Mat_khau], function (err, rows) {
+        if (rows.length > 0) {
+            req.session.manager = rows[0];
+            res.redirect('/manager/home');
+            return;
+
+        } else {
+            var vm = {
+                showError: true,
+                errorMsg: 'Login failed'
+            };
+            res.render('manager/login', vm);
+        }
+    });
+});
+
+router.get('/home', restrict, (req, res) => {
+    var vm = {
+        managerName: req.session.manager.Ten_quan_ly
+    };
+    res.render('manager/home', vm);
+});
+
+
+
+router.get('/update_product', restrict, (req, res) => {
+    xu_ly.connectDatabase().query("select * from may_anh where Hang = 'Canon'", function (err, rows) {
+        if (err) throw err;
+        var vm = {
+            products: rows,
+            managerName: req.session.manager.Ten_quan_ly
+        };
+        res.render('manager/update_product', vm);
+    })
+
+
+});
+
+router.get('/update_product/:id', restrict, (req, res) => {
+    var pid = req.params.id;
+    xu_ly.connectDatabase().query("select * from may_anh where Hang = ?", [pid], function (err, rows) {
+        if (err) throw err;
+        var vm = {
+            products: rows,
+            managerName: req.session.manager.Ten_quan_ly
+        };
+        res.render('manager/update_product', vm)
+    })
+
+});
+
+router.get('/edit?', restrict, (req, res) => {
+    var Ma_so = req.query.id;
+    xu_ly.connectDatabase().query("select * from may_anh where Ma_so = ?", [Ma_so], function (err, rows) {
+        if (err) throw err;
+        var vm = {
+            product: rows[0],
+            check: false,
+            managerName: req.session.manager.Ten_quan_ly
+        };
+        res.render('manager/edit', vm)
+    })
+
+
+});
+
+router.post('/edit', (req, res) => {
+    var Don_gia_ban = req.body.Don_gia_ban;
+    var Mo_ta = req.body.Mo_ta;
+    var Ma_so = req.body.Ma_so;
+    xu_ly.connectDatabase().query("update may_anh set Don_gia_ban = ? , Mo_ta = ? Where Ma_so=?", [Don_gia_ban, Mo_ta, Ma_so], function (err, rows) {
+        if (err) throw err;
+        var vm = {
+            check: true
+        }
+        res.render('manager/edit', vm)
+    })
+});
+
+router.get('/delete?', restrict, (req, res) => {
+    var Ma_so = req.query.id;
+    var Hang = req.query.brand;
+    xu_ly.connectDatabase().query("delete from may_anh where Ma_so = ?", [Ma_so], function (err, row) {
+        if (err) throw err;
+        xu_ly.connectDatabase().query("Select * from may_anh where Hang = ?", [Hang], function (err, rows) {
+            var vm = {
+                products: rows,
+                managerName: req.session.manager.Ten_quan_ly
+            };
+            res.render('manager/update_product', vm)
+        })
+    })
+});
+
+router.get('/add_product', restrict, (req, res) => {
+    var vm = {
+        managerName: req.session.manager.Ten_quan_ly
+    }
+    res.render('manager/add_product', vm);
+
+});
+
+router.post('/add_product', upload.single('Hinh_anh'), (req, res) => {
+    if (req.fileValidationError) {
+        return res.end(req.fileValidationError);
+    }
+    const productdata = {
+        Ten: req.body.Ten,
+        Don_gia_ban: req.body.Don_gia_ban,
+        Hang: req.body.Hang,
+        Loai: req.body.Loai,
+        Mo_ta: req.body.Mo_ta,
+        Hinh_anh: req.file.originalname
+    }
+    xu_ly.connectDatabase().query("INSERT INTO may_anh SET ?", productdata, function (err, row) {
+        if (err) throw err;
+        var vm = {
+            successResult: true
+        }
+        res.render('manager/add_product', vm);
+        return;
+    })
+});
+
+router.get('/update_order', restrict, (req, res) => {
+    var sql = "SELECT hoa_don.Ma_hoa_don, DATE_FORMAT(hoa_don.Ngay_dat_hang, '%Y-%m-%d') AS Ngay_dat_hang, hoa_don.Trang_thai, hoa_don.Tong_tien, khach_hang.Ten_khach_hang FROM hoa_don JOIN khach_hang ON hoa_don.Ma_khach_hang = khach_hang.Ma_khach_hang ORDER BY hoa_don.Ngay_dat_hang DESC"
+    xu_ly.connectDatabase().query(sql, function (err, rows) {
+        var vm = {
+            order: rows,
+            managerName: req.session.manager.Ten_quan_ly
+        }
+        res.render('manager/update_order', vm);
+    })
+});
+
+router.get('/updateOrder?', restrict, (req, res) => {
+    var Ma_hoa_don = req.query.id;
+    var Trang_thai = req.query.newstt;
+    xu_ly.connectDatabase().query("update hoa_don set Trang_thai = ? Where Ma_hoa_don=?", [Trang_thai, Ma_hoa_don], function (err, row) {
+        if (err) throw err;
+        var sql = "SELECT hoa_don.Ma_hoa_don, DATE_FORMAT(hoa_don.Ngay_dat_hang, '%Y-%m-%d') AS Ngay_dat_hang, hoa_don.Trang_thai, hoa_don.Tong_tien, khach_hang.Ten_khach_hang FROM hoa_don JOIN khach_hang ON hoa_don.Ma_khach_hang = khach_hang.Ma_khach_hang ORDER BY hoa_don.Ngay_dat_hang DESC"
+        xu_ly.connectDatabase().query(sql, function (err, rows) {
+            var vm = {
+                order: rows,
+                managerName: req.session.manager.Ten_quan_ly
+            }
+            res.render('manager/update_order', vm);
+        })
+
+    })
+});
+
+router.post('/logout', (req, res) => {
+    req.session.manager = null;
+    res.redirect("/");
+});
+module.exports = router;
---
+++ controllers/userController.js
@@ -0,0 +1,304 @@
+var express = require('express'),
+    productRepo = require('../repos/productRepo');
+var restrict = require('../middle-wares/restrictUser');
+var xu_ly = require('../fn/xu_ly');
+var request = require('request');
+var config = require('../config/config');
+var SHA256 = require('crypto-js/sha256');
+var router = express.Router();
+
+
+router.get('/login', (req, res) => {
+    res.render('user/login')
+});
+
+router.post('/login', (req, res) => {
+    var user = {
+        Email: req.body.Email,
+        Mat_khau: SHA256(req.body.Mat_khau).toString()
+    };
+
+    xu_ly.connectDatabase().query("select * from khach_hang where Email = ? AND Mat_khau = ?", [user.Email, user.Mat_khau], function (err, rows) {
+        if (rows.length > 0) {
+            req.session.user = rows[0];
+            req.session.cart = [];
+            var url = '/user/home';
+            if (req.query.retUrl) {
+                url = req.query.retUrl;
+            }
+            res.redirect(url);
+            return;
+
+        } else {
+            var vm = {
+                showError: true,
+                errorMsg: 'Login failed'
+            };
+            res.render('user/login', vm);
+        }
+    });
+});
+
+
+router.get('/home', restrict, (req, res) => {
+    var vm = {
+
+    };
+    productRepo.loadMostviewed().then(rows => {
+        vm.mostviewed1 = xu_ly.chuoi1(rows)
+        vm.mostviewed2 = xu_ly.chuoi2(rows)
+        vm.mostviewed3 = xu_ly.chuoi3(rows)
+
+        productRepo.loadBestselling().then(rows => {
+            vm.bestselling1 = xu_ly.chuoi1(rows)
+            vm.bestselling2 = xu_ly.chuoi2(rows)
+            vm.bestselling3 = xu_ly.chuoi3(rows)
+
+            productRepo.loadLatest().then(rows => {
+                vm.latest = rows
+                res.render('user/home', vm);
+            });
+
+        });
+    });
+});
+
+
+
+router.get('/products/:id', restrict, (req, res) => {
+    var pid = req.params.id;
+
+    var page = req.query.page;
+
+    if (!page) {
+        page = 1;
+    }
+
+    var offset = (page - 1) * config.PRODUCTS_PER_PAGE;
+
+    var p1 = productRepo.loadAllByID(pid, offset);
+    var p2 = productRepo.countByID(pid);
+    Promise.all([p1, p2]).then(([pRows, countRows]) => {
+
+        var total = countRows[0].total;
+        var nPages = total / config.PRODUCTS_PER_PAGE;
+        if (total % config.PRODUCTS_PER_PAGE > 0) {
+            nPages++;
+        }
+
+        var numbers = [];
+        for (i = 1; i <= nPages; i++) {
+            numbers.push({
+                value: i,
+                isCurPage: i === +page
+            });
+        }
+
+        var vm = {
+            products: pRows,
+            noProducts: pRows.length === 0,
+            page_numbers: numbers,
+            amount: total,
+            title: pid,
+
+        };
+        res.render('user/products', vm)
+    })
+});
+
+router.get('/products?', restrict, (req, res) => {
+    var price = req.query.price;
+    var name = req.query.name;
+    var page = req.query.page;
+    if (!page) {
+        page = 1;
+    }
+    var offset = (page - 1) * config.PRODUCTS_PER_PAGE;
+
+    var p1 = productRepo.loadAllByNP(price, name, offset);
+    var p2 = productRepo.countByNP(price, name);
+    Promise.all([p1, p2]).then(([pRows, countRows]) => {
+
+        var total = countRows[0].total;
+        var nPages = total / config.PRODUCTS_PER_PAGE;
+        if (total % config.PRODUCTS_PER_PAGE > 0) {
+            nPages++;
+        }
+
+        var numbers = [];
+        for (i = 1; i <= nPages; i++) {
+            numbers.push({
+                value: i,
+                isCurPage: i === +page,
+                price: price,
+                name: name,
+                isSearch: true
+            });
+        }
+        var vm = {
+            products: pRows,
+            noProducts: pRows.length === 0,
+            page_numbers: numbers,
+            amount: total,
+            title: "Products found",
+
+        };
+
+        res.render('user/products', vm)
+    })
+});
+
+router.get('/product_details/:id', restrict, (req, res) => {
+    var pid = req.params.id;
+    var vm = {
+
+    };
+    xu_ly.connectDatabase().query("select * from may_anh where Ma_so = ?;", [pid], function (err, row) {
+        if (err) throw err;
+        vm.product = row[0]
+
+        xu_ly.connectDatabase().query("select * from may_anh WHERE Hang= ? AND Ma_so != ? ORDER BY Rand() limit 5", [row[0].Hang, pid], function (err, rows) {
+            if (err) throw err;
+            vm.relatedBrand = rows
+
+            xu_ly.connectDatabase().query("select * from may_anh WHERE Loai= ? AND Ma_so != ? ORDER BY Rand() limit 5", [row[0].Loai, pid], function (err, rows) {
+                if (err) throw err;
+                vm.relatedType = rows
+
+                res.render('user/product_details', vm)
+            })
+        })
+    })
+});
+
+
+router.post('/addItem', (req, res) => {
+    var cart = req.session.cart
+    if (cart.indexOf(req.body.Ma_so) == -1) {
+        cart.push(req.body.Ma_so);
+    }
+    res.redirect(req.headers.referer);
+})
+
+router.get('/cart', (req, res) => {
+
+    if (req.session.cart.length == 0) {
+        res.redirect(req.headers.referer);
+    } else {
+        var arr_p = [];
+        for (var i = 0; i < req.session.cart.length; i++) {
+            var cartItem = req.session.cart[i];
+            var p = productRepo.single(cartItem);
+            arr_p.push(p);
+        }
+
+        var items = [];
+        Promise.all(arr_p).then(result => {
+            for (var i = result.length - 1; i >= 0; i--) {
+                var pro = result[i][0];
+                var item = {
+                    Product: pro
+                };
+                items.push(item);
+            }
+
+            var vm = {
+                items: items
+            };
+            res.render('user/cart', vm);
+        });
+    }
+
+});
+
+router.post('/removeItem', (req, res) => {
+    var cart = req.session.cart
+    for (var i = cart.length - 1; i >= 0; i--) {
+        if (req.body.Ma_so == cart[i]) {
+            cart.splice(i, 1);
+        }
+    }
+    res.redirect(req.headers.referer);
+});
+
+router.post('/addOrder', (req, res) => {
+    var Tong_tien = req.body.totalPrice
+    delete req.body.totalPrice
+
+    var order = {
+        Tong_tien: Tong_tien,
+        Ma_khach_hang: req.session.user.Ma_khach_hang
+    }
+    xu_ly.connectDatabase().query("INSERT INTO hoa_don SET ?", order, function (err, row) {
+        if (err) throw err;
+        xu_ly.connectDatabase().query("SELECT max(Ma_hoa_don) as Ma_hoa_don FROM hoa_don", function (err, row) {
+            if (err) throw err;
+            for (var key in req.body) {
+                item = req.body[key];
+                var order_detail = {
+                    Ma_hoa_don: row[0].Ma_hoa_don,
+                    Ma_so: key,
+                    So_luong: item
+                }
+                xu_ly.connectDatabase().query("INSERT INTO ct_hoa_don SET ?", order_detail, function (err, row) {
+                    if (err) throw err;
+                })
+            }
+            var vm = {
+                success: true
+            }
+            res.render('user/cart', vm);
+        })
+    })
+
+});
+
+
+router.get('/purchase_history', (req, res) => {
+    var sql="SELECT Ma_hoa_don, DATE_FORMAT(Ngay_dat_hang, '%Y-%m-%d') AS Ngay_dat_hang, Trang_thai, Tong_tien FROM hoa_don where Ma_khach_hang = ? ORDER BY Ngay_dat_hang DESC"
+    xu_ly.connectDatabase().query(sql, req.session.user.Ma_khach_hang , function (err, rows) {
+        if (err) throw err;
+        var vm={
+            order: rows
+        }
+        res.render('user/purchase_history', vm);       
+    })
+});
+
+router.get('/profile', (req, res) => {
+    var sql="SELECT *,DATE_FORMAT(Ngay_sinh, '%Y-%m-%d') AS Ngay_sinh from khach_hang where Ma_khach_hang = ?"
+    xu_ly.connectDatabase().query(sql, req.session.user.Ma_khach_hang , function (err, rows) {
+        if (err) throw err;
+        var vm={
+            user: rows[0]
+        }
+        res.render('user/profile', vm);       
+    })
+});
+
+router.post('/profile', (req, res) => {
+    const userdata = [
+        Email= req.body.Email.toLowerCase(),
+        Ten_khach_hang= req.body.Ten_khach_hang,
+        Gioi_tinh= req.body.Gioi_tinh,
+        Ngay_sinh= req.body.Ngay_sinh,
+        So_dien_thoai= req.body.So_dien_thoai,
+        Dia_chi= req.body.Dia_chi,
+        Ma_khach_hang = req.session.user.Ma_khach_hang
+    ]
+    var sql="update khach_hang set Email = ?, Ten_khach_hang = ?, Gioi_tinh=?, Ngay_sinh=?, So_dien_thoai=?, Dia_chi=? where Ma_khach_hang = ?"
+    xu_ly.connectDatabase().query(sql ,userdata, function (err, rows) {
+        if (err) throw err;
+        var vm={
+            success: true
+        }
+        res.render('user/profile', vm);       
+    })
+});
+
+router.post('/logout', (req, res) => {
+    req.session.user = null;
+    req.session.cart = [];
+    res.redirect("/customer/home");
+});
+module.exports = router;
